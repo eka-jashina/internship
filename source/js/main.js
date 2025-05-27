2d881557ba6c002526175262cb754328dd7d1531
eka-jashina <142108910+eka-jashina@users.noreply.github.com>
2025-05-27 12:44:22 +0000

    add programs

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,6 @@
 import './modules/nav.js';
-import { initHeroSwiper } from './modules/hero-swiper.js'
+import { initHeroSwiper } from './modules/hero-swiper.js';
+import { initProgramsSwiper } from './modules/programs-swiper.js';
 
 initHeroSwiper();
+initProgramsSwiper();
---
+++ source/js/modules/programs-swiper.js
@@ -0,0 +1,31 @@
+import Swiper from 'swiper';
+import {Navigation, Pagination } from 'swiper/modules';
+
+function initProgramsSwiper() {
+  const programsSwiper = new Swiper('.programs__swiper', {
+    modules: [Navigation, Pagination],
+    speed: 300,
+    autoHeight: true,
+    slidesPerView: 3,
+    spaceBetween: 32,
+
+    pagination: {
+      el: '.programs__pagination',
+      type: "progressbar",
+    },
+
+    navigation: {
+      nextEl: '.programs__button--next',
+      prevEl: '.programs__button--prev',
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
+
+  return programsSwiper;
+}
+
+export { initProgramsSwiper };
