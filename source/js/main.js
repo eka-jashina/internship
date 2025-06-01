d91938e2f68d3ec234597e4452de46560e43af97
eka-jashina <eka.jashina@gmail.com>
2025-06-01 20:32:01 +0300

    add select and nebo

--- source/js/main.js
+++ source/js/main.js
@@ -1,22 +1,27 @@
-import './modules/nav.js';
-import { initHeroSwiper } from './modules/hero-swiper.js';
-import { initProgramsSwiper } from './modules/programs-swiper.js';
-import { toggleAccordion, openAccordion } from './modules/accordion.js';
-import { initReviewsSwiper } from './modules/reviews-swiper.js'
+import "./modules/nav.js";
+import { initHeroSwiper } from "./modules/hero-swiper.js";
+import { initProgramsSwiper } from "./modules/programs-swiper.js";
+import { toggleAccordion, openAccordion } from "./modules/accordion.js";
+import { initReviewsSwiper } from "./modules/reviews-swiper.js";
+import "./vendor/slimselect.min.js";
 
-const buttonsToOpen = document.querySelectorAll('.accordion-button[data-open]');
+const buttonsToOpen = document.querySelectorAll(".accordion-button[data-open]");
 
 initHeroSwiper();
 initProgramsSwiper();
 initReviewsSwiper();
 
-document.addEventListener('click', function(e) {
-  if (e.target.closest('.accordion-button')) {
+document.addEventListener("click", function (e) {
+  if (e.target.closest(".accordion-button")) {
     toggleAccordion(e.target);
   }
 });
 
-buttonsToOpen.forEach(button => openAccordion(button));
+buttonsToOpen.forEach((button) => openAccordion(button));
 
-
-
+new SlimSelect({
+  select: '#city',
+  settings: {
+    showSearch: false,
+  }
+});
